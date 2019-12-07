d7be5c3c42edf5e0a3592b2ce671bc8003629d74
Matty Smith <matthewcsmith18@gmail.com>
2019-12-06 21:12:05 -0500

    f

---
+++ src/components/Budget-item-list.tsx
@@ -0,0 +1,25 @@
+import * as React from 'react'
+
+import { BudgetItemObjInterface, BudegetListInterface } from '../interfaces'
+
+import BudgetItem from './budget-item'
+
+const BudgetList = (props: BudegetListInterface) => {
+  return (
+    <div className="budget-list">
+      {props.budgetItems.map((item: BudgetItemObjInterface) => {
+        return (
+          <BudgetItem
+            key={item.id}
+            budgetCurrency={props.budgetCurrency}
+            budgetItem={item}
+            handleItemUpdate={props.handleItemUpdate}
+            handleItemRemove={props.handleItemRemove}
+          />
+        )
+      })}
+    </div>
+  )
+}
+
+export default BudgetList
