16ea82111bdf7187407f52b1039569195aaf8828
Matty Smith <matthewcsmith18@gmail.com>
2019-12-06 22:13:42 -0500

    components, data, pages

---
+++ src/components/budget-total.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react'
+import { BudgetTotalInterface } from './../interfaces'
+
+const BudgetTotal = (props: BudgetTotalInterface) => {
+  return (
+    <div className="budget-total">
+      <h2>
+        <span className="budget-period">Your {props.budgetPeriod}</span>
+        {' '}
+        <span className="budget-label">budget:</span>
+        {' '}
+        <span
+          className={`budget-total ${props.budgetAmount - props.budgetPaid > 0 ? 'budget-total-positive' : 'budget-total-negative'}`}>
+          {props.budgetAmount - props.budgetPaid}
+        </span>
+        {' '}
+        <span className="budget-currency">{props.budgetCurrency}</span>
+      </h2>
+    </div>
+  )
+}
+
+export default BudgetTotal
